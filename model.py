a49b8ed7146299f2875c5d6b0f8190df8ed48aec
Anh Nguyen <quanhwalker1999@gmail.com>
2021-06-18 16:42:27 +0900

    Added baseline models submissions and model initialization

---
+++ model.py
@@ -0,0 +1,73 @@
+from sklearn.linear_model import ElasticNet, Lasso, BayesianRidge, LassoLarsIC
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import RobustScaler
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.metrics import mean_squared_error
+import lightgbm as lgb
+import xgboost as xgb
+
+import numpy as np
+
+# Cross validation score setup
+n_folds = 5
+def rmse_cv(model, n_folds, X_train, y_train):
+    kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(X_train)
+    rmse = np.sqrt(-cross_val_score(model, X_train, y_train, scoring='neg_mean_squared_error', cv=kf))
+
+    return (rmse)
+
+#Initialize base models
+lasso = make_pipeline(RobustScaler(), Lasso(alpha=0.0005, random_state=42, tol=1e-1))
+ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=0.9, random_state=3))
+GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
+                                   max_depth=4, max_features='sqrt',
+                                   min_samples_leaf=15, min_samples_split=10,
+                                   loss='huber', random_state =5)
+ridge = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
+xgb_model = xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468,
+                             learning_rate=0.05, max_depth=3,
+                             min_child_weight=1.7817, n_estimators=2200,
+                             reg_alpha=0.4640, reg_lambda=0.8571,
+                             subsample=0.5213, silent=1,
+                             random_state =7, nthread = -1)
+lgb_model = lgb.LGBMRegressor(objective='regression',num_leaves=5,
+                              learning_rate=0.05, n_estimators=720,
+                              max_bin = 55, bagging_fraction = 0.8,
+                              bagging_freq = 5, feature_fraction = 0.2319,
+                              feature_fraction_seed=9, bagging_seed=9,
+                              min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
+# KFold test
+score = rmse_cv(GBoost,5, X_train, y_train)
+print(" Averaged base models score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+
+
+#Average models
+class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, models):
+        self.models = models
+
+    # we define clones of the original models to fit the data in
+    def fit(self, X, y):
+        self.models_ = [clone(x) for x in self.models]
+
+        # Train cloned base models
+        for model in self.models_:
+            model.fit(X, y)
+
+        return self
+
+    # Now we do the predictions for cloned models and average them
+    def predict(self, X):
+        predictions = np.column_stack([
+            model.predict(X) for model in self.models_
+        ])
+        return np.mean(predictions, axis=1)
+
+avg_models = AveragingModels(models=(xgb_model, lgb_model, model, GBoost))
+score = rmse_cv(avg_models, 5, X_train, y_train)
+
+avg_models.fit(X_train, y_train)
+pred_stacking = avg_models.predict(X_test)
